f11ebe7109ed7e47f1c5b1d95e795e39efe832d4
Craig Storer <craigstorer@gmail.com>
2020-03-20 01:18:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
-# Make your shoe class here!
+class Shoe 
+  
+    
+  
+  attr_accessor :color, :size , :material, :condition
+
+  
+  def initialize(brand) 
+    @brand = brand
+  end
+  
+  def brand
+    @brand 
+  end
+  
+
+  def cobble
+    # if condition == "new"
+    puts "Your shoe is as good as new!"
+    # else
+    #   "tattered"
+    @cobble
+  end
+end
+
+  
+
